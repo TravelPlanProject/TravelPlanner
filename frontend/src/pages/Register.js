2c7bbdb6075259a8024ace7569f55a3009231d5c
최혜림 <chhari0708@naver.com>
2023-02-03 00:09:58 +0900

    Login, Register 페이지 수정

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -6,10 +6,10 @@ function Register() {
         <p id="title">회원가입</p>
         <div>
           <form>
-            <input id="userId" type="text" />
-            <input id="userPw" type="password" />
-            <input id="userPwc" type="password" />
-            <input id="userEmail" />
+            <input id="userId" type="text" placeholder="아이디를 입력하세요" />
+            <input id="userPw" type="password" placeholder="비밀번호를 입력하세요" />
+            <input id="userPwc" type="password" placeholder="비밀번호를 한 번 더 입력하세요" />
+            <input id="userEmail" placeholder="이메일을 입력하세요" />
             <button>회원가입</button>
           </form>
         </div>
