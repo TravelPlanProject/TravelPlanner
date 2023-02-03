7f00a884dbb946cc24dc9d7c3d800eb8bb592bae
최혜림 <chhari0708@naver.com>
2023-02-04 01:18:22 +0900

    회원가입 창 퍼블리싱완료
    해야하는거
    - 카카오톡 회원가입
    - 타 페이지 광고

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,27 +1,32 @@
-import '../styles/Register.css';
+import styles from '../styles/Register.module.css';
 
 function Register() {
   return (
     <div>
-      <div className="wrap">
-        <div className="logo"></div>
-        <p className="title">회원가입</p>
-        <div className="form">
+      <div className={styles.wrap}>
+        <div className={styles.logo}></div>
+        <p className={styles.title}>회원가입</p>
+        <div className={styles.form}>
           <form>
-            <div className="id_box">
-              <input className="input_id" id="userId" type="text" placeholder="아이디를 입력하세요" />
+            <div className={styles.id_box}>
+              <input className={styles.input_id} id="userId" type="text" placeholder="아이디를 입력하세요" />
             </div>
-            <div className="pw_box">
-              <input className="input_pw" id="userPw" type="password" placeholder="비밀번호를 입력하세요" />
+            <div className={styles.pw_box}>
+              <input className={styles.input_pw} id="userPw" type="password" placeholder="비밀번호를 입력하세요" />
             </div>
-            <div className="pwc_box">
-              <input className="input_pwc" id="userPwc" type="password" placeholder="비밀번호를 한 번 더 입력하세요" />
+            <div className={styles.pwc_box}>
+              <input
+                className={styles.input_pwc}
+                id="userPwc"
+                type="password"
+                placeholder="비밀번호를 한 번 더 입력하세요"
+              />
             </div>
-            <div className="email_box">
-              <input className="input_email" id="userEmail" placeholder="이메일을 입력하세요" />
+            <div className={styles.email_box}>
+              <input className={styles.input_email} id="userEmail" placeholder="이메일을 입력하세요" />
             </div>
-            <div className="Btn_box">
-              <button>회원가입</button>
+            <div>
+              <button className={styles.submitBtn}>회원가입</button>
             </div>
           </form>
         </div>
