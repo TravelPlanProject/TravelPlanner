8963a46a9c18b3158fd977117a05af535bdd2968
최혜림 <chhari0708@naver.com>
2023-02-04 01:03:43 +0900

    css 적용 방법 수정

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,28 +1,28 @@
 import Register from './Register';
 import { Link } from 'react-router-dom';
-import '../styles/Login.css';
+import styles from '../styles/Login.module.css';
 function Login() {
   return (
     <div>
-      <div id="wrap">
-        <div className="logo"></div>
-        <div className="title">로그인</div>
-        <div className="form">
+      <div className={styles.wrap}>
+        <div className={styles.logo}></div>
+        <div className={styles.title}>로그인</div>
+        <div className={styles.form}>
           <form>
-            <div className="id_box">
-              <input id="userId" className="input_id" placeholder="아이디를 입력해주세요" />
+            <div className={styles.id_box}>
+              <input id="userId" className={styles.input_id} placeholder="아이디를 입력해주세요" />
             </div>
-            <div className="pw_box">
-              <input type="password" id="userPw" className="input_pw" placeholder="비밀번호를 입력해주세요" />
+            <div className={styles.pw_box}>
+              <input type="password" id="userPw" className={styles.input_pw} placeholder="비밀번호를 입력해주세요" />
             </div>
-            <div className="Btn_box">
-              <button id="submitBtn" className="submitBtn">
+            <div className={styles.Btn_box}>
+              <button id="submitBtn" className={styles.submitBtn}>
                 로그인
               </button>
             </div>
           </form>
 
-          <div className="Register">
+          <div className={styles.Register}>
             <Link to="/Register" element={<Register />}>
               회원가입
             </Link>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,16 +1,28 @@
+import '../styles/Register.css';
+
 function Register() {
   return (
     <div>
-      <div>
-        <div id="logo"></div>
-        <p id="title">회원가입</p>
-        <div>
+      <div className="wrap">
+        <div className="logo"></div>
+        <p className="title">회원가입</p>
+        <div className="form">
           <form>
-            <input id="userId" type="text" placeholder="아이디를 입력하세요" />
-            <input id="userPw" type="password" placeholder="비밀번호를 입력하세요" />
-            <input id="userPwc" type="password" placeholder="비밀번호를 한 번 더 입력하세요" />
-            <input id="userEmail" placeholder="이메일을 입력하세요" />
-            <button>회원가입</button>
+            <div className="id_box">
+              <input className="input_id" id="userId" type="text" placeholder="아이디를 입력하세요" />
+            </div>
+            <div className="pw_box">
+              <input className="input_pw" id="userPw" type="password" placeholder="비밀번호를 입력하세요" />
+            </div>
+            <div className="pwc_box">
+              <input className="input_pwc" id="userPwc" type="password" placeholder="비밀번호를 한 번 더 입력하세요" />
+            </div>
+            <div className="email_box">
+              <input className="input_email" id="userEmail" placeholder="이메일을 입력하세요" />
+            </div>
+            <div className="Btn_box">
+              <button>회원가입</button>
+            </div>
           </form>
         </div>
       </div>
